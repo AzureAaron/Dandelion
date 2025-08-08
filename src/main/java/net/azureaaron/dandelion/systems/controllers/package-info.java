4d84675de3301c52f31ec7fa20be51eaf561e7bd
Aaron <51387595+AzureAaron@users.noreply.github.com>
2025-08-08 01:49:17 -0400

    Add nullability annotations to some packages
    The nullability rules did not change, this is just to better inform
    people of them.

--- src/main/java/net/azureaaron/dandelion/systems/controllers/package-info.java
+++ src/main/java/net/azureaaron/dandelion/systems/controllers/package-info.java
@@ -1,4 +1,7 @@
 /**
  * Package containing the default provided {@code Option} type controllers.
  */
+@NotNullByDefault
 package net.azureaaron.dandelion.systems.controllers;
+
+import org.jetbrains.annotations.NotNullByDefault;
--- src/main/java/net/azureaaron/dandelion/systems/package-info.java
+++ src/main/java/net/azureaaron/dandelion/systems/package-info.java
@@ -1,4 +1,7 @@
 /**
  * Package containing the core Dandelion API.
  */
+@NotNullByDefault
 package net.azureaaron.dandelion.systems;
+
+import org.jetbrains.annotations.NotNullByDefault;
